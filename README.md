6c083117f724829bb423ed5bb3a2e7495d1d626e
k <k@localhost.com>
2022-07-30 18:17:23 +0900

    클래스 컴포넌트 -> 함수 컴포넌트

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ yarn create react-app app-name
 * */public*, */src* 디렉토리만 남기고 내용파일들 삭제.
 * package.json에 있는 패키지 중에 사욯하지 않는 것은 제거 할 수도 있지만 각각의 패키지 용도에 대한 고민은 나중으로...
 
-## 기본 파일 추가
+## 기본 파일 추가 [v0.0.1](https://github.com/jihoekim/wanted-pre-onboarding-challenge-fe-1/releases/tag/v0.0.1)
 
 * */public/index.html*
 * */src/index.{css,js}* 
--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./app.css";
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <div className="App">
+        <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+        </div>
     );
-  }
 }
 
 export default App;
